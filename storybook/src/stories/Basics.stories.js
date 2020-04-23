3c3505b970de69df688e371f6f8cdf97ebfece26
peterkabai <peterkabai@me.com>
2020-04-23 08:30:33 -0700

    Adds to TF version story

--- storybook/src/stories/Basics.stories.js
+++ storybook/src/stories/Basics.stories.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Button } from '@storybook/react/demo';
+
+// this would be a lot faster with @tensorflow/tfjs-node
 import * as tf from '@tensorflow/tfjs';
+
 
 export default {
   title: 'Basics'
 };
 
-export const TensorFlowVersion = () => (
-  <p>The current version of TensorFlow JS is {tf.version.tfjs}</p>
-);
+export const TensorFlowVersion = () => {
+
+  // this initializes the TF backend
+  tf.backend()
+
+  return (
+    <>
+      <p>Version: {tf.version.tfjs}</p>
+      <p>Backend: {tf.getBackend()}</p>
+      <a 
+        href='https://playground.tensorflow.org/'
+        target="_blank"
+      >
+        TensorFlow Playground
+      </a>
+      <br />
+      <a 
+        href='https://js.tensorflow.org/api/latest/'
+        target="_blank"
+      >
+        TensorFlow API
+      </a>
+    </>
+  );
+}
